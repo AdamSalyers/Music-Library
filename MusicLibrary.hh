ca65c2a205fe8e78dfb44b4b89fc4f4c12662437
AdamSalyers <46688158+AdamSalyers@users.noreply.github.com>
2019-04-28 15:54:30 -0600

    Update MusicLibrary.hh

--- MusicLibrary.hh
+++ MusicLibrary.hh
@@ -2,11 +2,14 @@
 #include <queue>
 #include <string>
 #include <vector>
-#include "Trie.cpp"
+#include <queue>
+//#include "MP3.cpp"
+#include "Trie.hpp"
+#include "Playlist.hpp"
 
 using namespace std;
 
-
+/*
 struct songNode {
 
   string name;
@@ -14,30 +17,98 @@ struct songNode {
   string genre;
   songNode* next;
   float priorityNum;
+  MP3 mysong;
 
   int length[2];
 };
-
-
+*/
 class MusicLibrary
 {
 public:
   MusicLibrary();
   ~MusicLibrary();
-  void insertSong();
-  void removeSong();
-  void displayPlaylist();
-  void skipSong();
-  songNode* search();
-  void createPlaylist();
+  void insertSong(string name, string artist, string genre, float priorityNum);
+  void removeSong(string name,string artist);
+  void displayPlaylist(string playlistName,bool x);
+  //void skipSong();
+  void goback();
+  void shuffle(string name);
+  void displayAllPlaylist();
+  songNode* search(string songName, string artist);
+  void createPlaylist(string name);
   void displayCurrentSong();
-  void addToQueue();
-  void removeFromQueue();
+  void addTooPlaylist(string playlistName,string songName,string artist);
+  void addToQueue(songNode* song);
+  void viewSongInfo(string songName, string artist);
+  //void removeFromQueue(songNode* song);
   void viewQueue();
+  songNode* getQueueSongnode(int i);
+  void displayTop5(string myword);
+  songNode* getTop5(int num);
+  //void playSong(songNode* song);
+  //void pauseSong();
 
 private:
   Trie mytrie;
   vector<Playlist> myplaylists;
   songNode* currsong;
   vector<songNode*> history;
+  queue<songNode*> myqueue;
+};
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+#include <queue>
+//#include "MP3.cpp"
+#include "Trie.hpp"
+#include "Playlist.hpp"
+
+using namespace std;
+
+/*
+struct songNode {
+
+  string name;
+  string artist;
+  string genre;
+  songNode* next;
+  float priorityNum;
+  MP3 mysong;
+
+  int length[2];
+};
+*/
+class MusicLibrary
+{
+public:
+  MusicLibrary();
+  ~MusicLibrary();
+  void insertSong(string name, string artist, string genre, float priorityNum);
+  void removeSong(string name,string artist);
+  void displayPlaylist(string playlistName,bool x);
+  //void skipSong();
+  void goback();
+  void shuffle(string name);
+  void displayAllPlaylist();
+  songNode* search(string songName, string artist);
+  void createPlaylist(string name);
+  void displayCurrentSong();
+  void addTooPlaylist(string playlistName,string songName,string artist);
+  void addToQueue(songNode* song);
+  void viewSongInfo(string songName, string artist);
+  //void removeFromQueue(songNode* song);
+  void viewQueue();
+  songNode* getQueueSongnode(int i);
+  void displayTop5(string myword);
+  songNode* getTop5(int num);
+  //void playSong(songNode* song);
+  //void pauseSong();
+
+private:
+  Trie mytrie;
+  vector<Playlist> myplaylists;
+  songNode* currsong;
+  vector<songNode*> history;
+  queue<songNode*> myqueue;
 };
